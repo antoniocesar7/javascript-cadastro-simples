e11ef23af67893870182493a6263a1b135674b38
antoniocesar7 <antoniocesarfernandes@gmail.com>
2023-05-25 13:50:38 -0300

    Deixando a forma mais primitiva de encontrar o item no array

--- telaBemVindos.js
+++ telaBemVindos.js
@@ -1,6 +1,7 @@
 let area = document.getElementById('area');
 let botao = document.getElementById("botao");
 nomeCadastro = ["Antonio César", "Fulano2", "Fulano3", "Fulano4"];
+let encontrou = false;
 
 function entrar() {
     let nome = prompt("Digite o seu nome");
@@ -9,15 +10,6 @@ function entrar() {
         alert("Ops, algo deu errado!!!");
         area.innerHTML = "Clique no botão para acessar";
     } else {
-        encontrou = nomeCadastro.find(element => element === nome);
-        console.log(encontrou);
-
-        const foundItem = nomeCadastro.find(element => element === nome);
-        if (foundItem) {
-            console.log("Item encontrado:", foundItem);
-        } else {
-            console.log("Item não encontrado no array.");
-        }
         for (let i = 0; i < 4; i++) {
             if (nomeCadastro[i] == nome) {
                 area.innerHTML = "Bem vindo " + nome + "<br>";
