b423aa6ad8a2995f6c33d890470527e36595a174
antoniocesar7 <antoniocesarfernandes@gmail.com>
2023-05-25 14:46:55 -0300

    Foi inserido tela de cadastro e quando enviar os nomes, ele limpa o elemento div area

--- telaBemVindos.js
+++ telaBemVindos.js
@@ -1,7 +1,54 @@
 let area = document.getElementById('area');
 let botao = document.getElementById("botao");
-nomeCadastro = ["Antonio César", "Fulano2", "Fulano3", "Fulano4"];
+let nomeCadastro = [];
 let encontrou = false;
+
+function telaCadastrar(){
+ for(i=0;i<5;i++){
+    //criando os labels
+    nomeLabel = document.createElement("label");
+    nomeLabel.innerHTML = "Nome: ";
+    area.appendChild(nomeLabel);
+
+    //criando as caixas de input
+    nomeInput = document.createElement("input");
+    nomeInput.setAttribute("type","text");
+    nomeInput.setAttribute("id","nome"+[i]);
+    area.appendChild(nomeInput);
+
+    //criando br para pular linha
+    pulaLinhaBr = document.createElement("br");
+    area.appendChild(pulaLinhaBr)
+
+ }
+    buttonEnviar = document.createElement("button");
+    buttonEnviar.innerText = "Enviar";
+    area.appendChild(buttonEnviar);
+    buttonEnviar.onclick = cadastrar;
+
+    botao.setAttribute("disabled", "true"); // Desabilita o botão "Cadastrar"
+}
+
+function cadastrar(){
+
+    for (i=0;i<5;i++){
+        nomeCadastro[i] = document.getElementById("nome"+i).value;
+    }
+    //console.log(nomeCadastro);
+    removerElementosCadastro();
+}
+
+function removerElementosCadastro() {
+    while (area.firstChild) {
+        area.removeChild(area.firstChild);
+    }
+
+    botao.removeAttribute("disabled"); // Habilita novamente o botão "Cadastrar"
+}
+
+// O método firstChild é uma propriedade de um elemento HTML que retorna o primeiro nó filho desse elemento. Um nó filho pode ser um elemento HTML, um texto, um comentário, etc.
+// No contexto do exemplo anterior, a propriedade firstChild é usada no loop while da função removerElementosCadastro() para obter o primeiro nó filho do elemento area e removê-lo. O loop continua removendo o primeiro nó filho repetidamente até que não haja mais nós filhos restantes, ou seja, até que o elemento area esteja vazio.
+// Desta forma, o uso de area.removeChild(area.firstChild) no loop permite remover todos os elementos filhos um por um até que não haja mais nenhum, limpando o conteúdo anteriormente adicionado pela função telaCadastrar().
 
 function entrar() {
     let nome = prompt("Digite o seu nome");
