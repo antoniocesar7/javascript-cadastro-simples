c9e9a97fd9242413f4e04f0d9516883165d2d835
antoniocesar7 <antoniocesarfernandes@gmail.com>
2023-05-25 13:33:08 -0300

    testando

--- teste.js
+++ teste.js
@@ -8,12 +8,24 @@
 //   console.log("Item não encontrado no array.");
 // }
 
-const array = [10, 20, 30, 40, 50];
-const item = 30;
+// const array = [10, 20, 30, 40, 50];
+// const item = 30;
 
-const foundItem = array.find(element => element === item);
-if (foundItem) {
-  console.log("Item encontrado:", foundItem);
-} else {
-  console.log("Item não encontrado no array.");
+// const foundItem = array.find(element => element === item);
+// if (foundItem) {
+//   console.log("Item encontrado:", foundItem);
+// } else {
+//   console.log("Item não encontrado no array.");
+// }
+ count = 17;
+ x = 1;
+ contador = 0;
+while(count > x){
+x*=3;
+count-=3;
+contador++;
 }
+console.log(contador);
+console.log(x);
+console.log(count);
+
