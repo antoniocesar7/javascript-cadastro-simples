25907cacea8faea33d586b4f763c2c88612b0c92
antoniocesar7 <antoniocesarfernandes@gmail.com>
2023-05-25 13:45:01 -0300

    Deixando somente o método find para encontrar item no array

--- telaBemVindos.js
+++ telaBemVindos.js
@@ -9,31 +9,15 @@ function entrar() {
         alert("Ops, algo deu errado!!!");
         area.innerHTML = "Clique no botão para acessar";
     } else {
-        encontrou = nomeCadastro.find(element => element === nome);
-        console.log(encontrou);
-
         const foundItem = nomeCadastro.find(element => element === nome);
         if (foundItem) {
-            console.log("Item encontrado:", foundItem);
+            document.write("Item encontrado:", foundItem);
         } else {
-            console.log("Item não encontrado no array.");
+            document.write("Item não encontrado, favor cadastrar");
         }
-        for (let i = 0; i < 4; i++) {
-            if (nomeCadastro[i] == nome) {
-                area.innerHTML = "Bem vindo " + nome + "<br>";
-                let botaoSair = document.createElement("button");
-                botaoSair.innerText = "Sair da conta";
-                botaoSair.onclick = sair;
-                area.appendChild(botaoSair);
-                botao.setAttribute("hidden", "true");
-                encontrou = true;
-            }
-        }
-
+        
     }
-    if (encontrou == false) {
-        alert("Nome não cadatrado, favor realizar cadastro!")
-    }
+    
 
 }//final da função entrar
 
